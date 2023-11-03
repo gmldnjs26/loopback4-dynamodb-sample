0a5835738357ea9f13decbdf8a33df0ef927bbea
gmldnjs26 <imleesky265@gmail.com>
2023-11-03 17:30:39 +0900

    fix: delete not use code

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,5 @@
-import {Count, CountSchema, repository, Where} from '@loopback/repository';
-import {post, param, get, getModelSchemaRef, requestBody} from '@loopback/rest';
+import {repository} from '@loopback/repository';
+import {param, get} from '@loopback/rest';
 import {User} from '../models';
 import {UserRepository} from '../repositories';
 
@@ -8,43 +8,6 @@ export class UserController {
     @repository(UserRepository)
     public userRepository: UserRepository,
   ) {}
-
-  @post('/users', {
-    responses: {
-      '200': {
-        description: 'User model instance',
-        content: {'application/json': {schema: getModelSchemaRef(User)}},
-      },
-    },
-  })
-  async create(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(User, {
-            title: 'NewUser',
-          }),
-        },
-      },
-    })
-    user: User,
-  ): Promise<User> {
-    console.log(user);
-    return this.userRepository.create(user);
-  }
-
-  @get('/users/count', {
-    responses: {
-      '200': {
-        description: 'User model count',
-        content: {'application/json': {schema: CountSchema}},
-      },
-    },
-  })
-  async count(@param.where(User) where?: Where<User>): Promise<Count> {
-    console.log('count');
-    return this.userRepository.count(where);
-  }
 
   @get('/users/{id}')
   async findById(@param.path.string('id') id: string): Promise<User> {
